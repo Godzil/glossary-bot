c5d02d37f1c65d758ad7ecef49c346fd4621fdd0
Godzil <godzil@godzil.net>
2019-06-06 16:17:11 +0100

    Update __init__

--- glossarybot/__init__.py
+++ glossarybot/__init__.py
@@ -1,2 +1,7 @@
-version = '4.2.0'
-source_url = 'https://github.com/ali1234/pimodisco'
+from . import formatter
+
+version = '0.1.0'
+source_url = 'https://github.com/Godzil/glossary-bot'
+
+# Exported
+Formatter = formatter.Formatter
