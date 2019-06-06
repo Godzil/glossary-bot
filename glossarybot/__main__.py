93862a6ac75dc748e36a6a6c8fd6b8e453715a05
Godzil <godzil@godzil.net>
2019-06-06 16:19:29 +0100

    Update __main__ to remove unneeded extensions and update a couple of things.

--- glossarybot/__main__.py
+++ glossarybot/__main__.py
@@ -1,51 +1,58 @@
 import logging
-logger = logging.getLogger(__name__)
-
 import aiohttp
-from importlib import import_module
-from configargparse import ArgumentParser
+import importlib
+import argparse
+
 from discord.ext.commands import Bot
 
-from pimodisco import source_url, version as version__
-from pimodisco.formatter import Formatter
+import glossarybot
 
+import glossarybot
 
-description="""
-    A good ol' Pimoroni Robot (Pirated, of course)
-    Version {}
+logger = logging.getLogger(__name__)
 
-    Commands should be prefixed with '{}' and are not case sensitive.
+description = """
+    One of the shiniest possible bot for discord: Glossary bot.
 
-    The source code for the Pimoroni Bot can be found here:
-        {}
+    Made from a pirated version of a good ol' Pimoroni Robot
+
+    Version {version}
+
+    Commands should be prefixed with '{prefix}' and are not case sensitive.
+
+    The source code for the Glossary-bot can be found here:
+        {base_url}
+
+    The original Pimoroni source can be found here:
+        {pimodispo_url}
 """
+
+ORIGIN_URL = "https://github.com/ali1234/pimodisco"
 
 extensions = [
     'pimodisco.filter',
     'pimodisco.checks',
     'pimodisco.commands',
-    'pimodisco.github',
-    'pimodisco.pinout',
-    'pimodisco.product',
-    'pimodisco.youtube',
 ]
 
 
 def main():
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', required=True, is_config_file=True, help='Config file path.')
-    parser.add_argument('-t', '--token', metavar='DISCORD_BOT_TOKEN', required=True, env_var='DISCORD_BOT_TOKEN', help='Discord bot token.')
-    parser.add_argument('-p', '--prefix', metavar='COMMAND_PREFIX', default='!', env_var='COMMAND_PREFIX', help='Command prefix.')
 
-    loaded_extensions = [import_module(e) for e in extensions]
+    loaded_extensions = [importlib.import_module(e) for e in extensions]
     for e in loaded_extensions:
-        e.setup_args(parser)
+        if e.setup_args:
+            e.setup_args(parser)
 
     args = parser.parse_args()
 
     bot = Bot(
-                command_prefix=args.prefix, formatter=Formatter(),
-                description=description.format(version__, args.prefix, source_url)
+                command_prefix=args.prefix, formatter=glossarybot.Formatter(),
+                description=description.format(version=glossarybot.version,
+                                               prefix=args.prefix,
+                                               base_url=glossarybot.source_url,
+                                               pimodiso_url=ORIGIN_URL)
              )
 
     with aiohttp.ClientSession() as session:
@@ -53,6 +60,7 @@ def main():
         for e in loaded_extensions:
             e.setup(bot, args)
         bot.run(args.token)
+
 
 if __name__ == '__main__':
     main()
